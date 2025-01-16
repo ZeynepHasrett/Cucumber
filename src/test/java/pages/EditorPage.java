62c0c26fdb551432202160c7882f078e90b1ee05
ZeynepHasrett <158554030+ZeynepHasrett@users.noreply.github.com>
2025-01-16 09:22:13 +0300

    day21

--- src/test/java/pages/EditorPage.java
+++ src/test/java/pages/EditorPage.java
@@ -1,7 +1,17 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class EditorPage {
 
-
+    public EditorPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    
+    @FindBy(xpath = "//*[@class='dt-button buttons-create']")
+    public WebElement newButonu;
 
 }

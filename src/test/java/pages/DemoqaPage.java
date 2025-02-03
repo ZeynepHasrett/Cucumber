b84aca66f6194fc6a07b3105098ddb50cd2242db
Zeynep Hasret Kırbaş <email@example.com>
2025-02-03 17:47:03 +0300

    version1

--- src/test/java/pages/DemoqaPage.java
+++ src/test/java/pages/DemoqaPage.java
@@ -1,7 +1,21 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class DemoqaPage {
 
+    public DemoqaPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 
+    @FindBy(xpath = "//span[.='Alerts']")
+    public WebElement alertsSekmesi;
+
+
+    @FindBy(id = "timerAlertButton")
+    public WebElement timerAlertButton;
 
 }

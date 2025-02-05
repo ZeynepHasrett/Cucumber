fec09a52b795178baf34293774fd4a35f38cae47
Zeynep Hasret Kırbaş <email@example.com>
2025-02-05 09:43:52 +0300

    v6

--- src/test/java/stepdefinitions/DemoqaStepdefinitions.java
+++ src/test/java/stepdefinitions/DemoqaStepdefinitions.java
@@ -7,14 +7,6 @@ public class DemoqaStepdefinitions {
 
     DemoqaPage demoqaPage = new DemoqaPage();
 
-    @Then("soldaki sekmelerden Alerts'e tiklar")
-    public void soldaki_sekmelerden_alerts_e_tiklar() {
-        demoqaPage.alertsSekmesi.click();
-    }
 
-    @When("On button click, alert will appear after {int} seconds karsisindaki click me butonuna basar")
-    public void on_button_click_alert_will_appear_after_seconds_karsisindaki_click_me_butonuna_basar(Integer int1) {
-        demoqaPage.timerAlertButton.click();
-    }
 
 }

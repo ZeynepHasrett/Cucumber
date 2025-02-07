7b35c609bbec31a9ee4790748e8735cee6513394
Zeynep Hasret Kırbaş <email@example.com>
2025-02-07 11:02:41 +0300

    v8

--- src/test/java/stepdefinitions/DemoqaStepdefinitions.java
+++ src/test/java/stepdefinitions/DemoqaStepdefinitions.java
@@ -1,7 +1,13 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.*;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.DemoqaPage;
+import utilities.Driver;
+
+import java.time.Duration;
 
 public class DemoqaStepdefinitions {
 
@@ -15,6 +21,22 @@ public class DemoqaStepdefinitions {
     @When("On button click, alert will appear after {int} seconds karsisindaki click me butonuna basar")
     public void on_button_click_alert_will_appear_after_seconds_karsisindaki_click_me_butonuna_basar(Integer int1) {
         demoqaPage.timerAlertButton.click();
+    }
+
+    @When("Alert’in gorunur olmasini bekler")
+    public void alert_in_gorunur_olmasini_bekler() {
+
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.alertIsPresent());
+    }
+
+    @Then("Alert uzerindeki yazinin {string} oldugunu test eder")
+    public void alert_uzerindeki_yazinin_oldugunu_test_eder(String expectedAlertYazi) {
+
+        String actualAlertYazi = Driver.getDriver().switchTo().alert().getText();
+
+        Assertions.assertEquals(expectedAlertYazi,actualAlertYazi);
+
     }
 
 }

0a752f10dfc4fc82ed3836e2e25d01e9ad044d84
Zeynep Hasret Kırbaş <email@example.com>
2025-02-17 11:46:30 +0300

    v12

--- src/test/java/stepdefinitions/DemoqaStepdefinitions.java
+++ src/test/java/stepdefinitions/DemoqaStepdefinitions.java
@@ -2,6 +2,7 @@ package stepdefinitions;
 
 import io.cucumber.java.en.*;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
